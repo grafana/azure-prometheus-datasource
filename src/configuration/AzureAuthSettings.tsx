2b61167ca3cde2de35fb9cae0718ee8cba939926
Andrew Hackmann <ajhack93@gmail.com>
2025-03-07 09:48:20 -0600

    more override stuff

--- src/configuration/AzureAuthSettings.tsx
+++ src/configuration/AzureAuthSettings.tsx
@@ -1,9 +1,7 @@
-import { cx } from '@emotion/css';
 import { AzureCredentials } from '@grafana/azure-sdk';
 import { DataSourceJsonData, DataSourceSettings } from '@grafana/data';
 import { config } from '@grafana/runtime';
-import { InlineField, InlineFieldRow, InlineSwitch, Input } from '@grafana/ui';
-import React, { FormEvent, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useEffectOnce } from 'react-use';
 
 import { getAzureCloudOptions, getCredentials, updateCredentials } from './AzureCredentialsConfig';
@@ -21,34 +19,11 @@ export interface HttpSettingsBaseProps<JSONData extends DataSourceJsonData = any
 export const AzureAuthSettings = (props: HttpSettingsBaseProps) => {
   const { dataSourceConfig, onChange } = props;
 
-  const [overrideAudienceAllowed] = useState<boolean>(!!config.featureToggles.prometheusAzureOverrideAudience);
-  const [overrideAudienceChecked, setOverrideAudienceChecked] = useState<boolean>(
-    !!dataSourceConfig.jsonData.azureEndpointResourceId
-  );
 
   const credentials = useMemo(() => getCredentials(dataSourceConfig), [dataSourceConfig]);
 
   const onCredentialsChange = (credentials: AzureCredentials): void => {
     onChange(updateCredentials(dataSourceConfig, credentials));
-  };
-
-  const onOverrideAudienceChange = (ev: FormEvent<HTMLInputElement>): void => {
-    setOverrideAudienceChecked(ev.currentTarget.checked);
-    if (!ev.currentTarget.checked) {
-      onChange({
-        ...dataSourceConfig,
-        jsonData: { ...dataSourceConfig.jsonData, azureEndpointResourceId: undefined },
-      });
-    }
-  };
-
-  const onResourceIdChange = (ev: FormEvent<HTMLInputElement>): void => {
-    if (overrideAudienceChecked) {
-      onChange({
-        ...dataSourceConfig,
-        jsonData: { ...dataSourceConfig.jsonData, azureEndpointResourceId: ev.currentTarget.value },
-      });
-    }
   };
 
   // The auth type needs to be set on the first load of the data source
