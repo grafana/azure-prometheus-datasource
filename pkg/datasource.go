a3419c485e4b815ad8d54b8d1c1d308619a5d4ad
Andrew Hackmann <ajhack93@gmail.com>
2025-04-01 13:32:15 -0500

    add logging tests

--- pkg/datasource.go
+++ pkg/datasource.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/grafana/grafana-azure-sdk-go/v2/azsettings"
 	"github.com/grafana/grafana-azure-sdk-go/v2/azusercontext"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
@@ -28,30 +30,92 @@ type Datasource struct {
 	Service *promlib.Service
 }
 
+func getQueryReqHeader(req *backend.QueryDataRequest, headerName string) string {
+	headerNameCI := strings.ToLower(headerName)
+
+	for name, value := range req.Headers {
+		if strings.ToLower(name) == headerNameCI {
+			return value
+		}
+	}
+
+	return ""
+}
+
+func getResourceReqHeader(req *backend.CallResourceRequest, headerName string) string {
+	headerNameCI := strings.ToLower(headerName)
+
+	for name, values := range req.Headers {
+		if strings.ToLower(name) == headerNameCI {
+			if len(values) > 0 {
+				return values[0]
+			} else {
+				return ""
+			}
+		}
+	}
+
+	return ""
+}
+
+func getCheckHealthReqHeader(req *backend.CheckHealthRequest, headerName string) string {
+	headerNameCI := strings.ToLower(headerName)
+
+	for name, value := range req.Headers {
+		if strings.ToLower(name) == headerNameCI {
+			return value
+		}
+	}
+
+	return ""
+}
+
+func logStuff(id string, logger log.Logger, endpoint string) {
+	claims := jwt.MapClaims{}
+	_, _, err := jwt.NewParser(jwt.WithValidMethods([]string{"ES256"})).ParseUnverified(id, claims)
+	if err != nil {
+		logger.Error("error:", err)
+	}
+	for r, i := range claims {
+		logger.Info("claim "+endpoint, fmt.Sprintf("key: %s value: %v", r, i))
+	}
+}
+
 func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	grafanaIdToken := getQueryReqHeader(req, "http_X-Grafana-Id")
+	logStuff(grafanaIdToken, backend.Logger.FromContext(ctx), "Query data")
 	ctx = d.contextualMiddlewares(ctx)
 	ctx = azusercontext.WithUserFromQueryReq(ctx, req)
 	return d.Service.QueryData(ctx, req)
 }
 
 func (d *Datasource) CallResource(ctx context.Context, req *backend.CallResourceRequest, sender backend.CallResourceResponseSender) error {
+	grafanaIdToken := getResourceReqHeader(req, "http_X-Grafana-Id")
+	logStuff(grafanaIdToken, backend.Logger.FromContext(ctx), "Call resource")
 	ctx = d.contextualMiddlewares(ctx)
 	ctx = azusercontext.WithUserFromResourceReq(ctx, req)
 	return d.Service.CallResource(ctx, req, sender)
 }
 
 func (d *Datasource) GetBuildInfo(ctx context.Context, req promlib.BuildInfoRequest) (*promlib.BuildInfoResponse, error) {
+	uc, _ := azusercontext.GetCurrentUser(ctx)
+	logStuff(uc.IdToken, backend.Logger.FromContext(ctx), "build info")
 	ctx = d.contextualMiddlewares(ctx)
+
 	return d.Service.GetBuildInfo(ctx, req)
 }
 
 func (d *Datasource) GetHeuristics(ctx context.Context, req promlib.HeuristicsRequest) (*promlib.Heuristics, error) {
+	uc, _ := azusercontext.GetCurrentUser(ctx)
+	logStuff(uc.IdToken, backend.Logger.FromContext(ctx), "heuristics")
 	ctx = d.contextualMiddlewares(ctx)
 	return d.Service.GetHeuristics(ctx, req)
 }
 
 func (d *Datasource) CheckHealth(ctx context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult,
 	error) {
+	grafanaIdToken := getCheckHealthReqHeader(req, "http_X-Grafana-Id")
+	logStuff(grafanaIdToken, backend.Logger.FromContext(ctx), "check health")
 	ctx = azusercontext.WithUserFromHealthCheckReq(ctx, req)
 	ctx = d.contextualMiddlewares(ctx)
 	return d.Service.CheckHealth(ctx, req)
