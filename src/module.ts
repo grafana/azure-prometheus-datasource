6dacf7c4a2e9e2ab7e80995fbbedab4a3fd8a005
Ashley Harrison <ashley.harrison@grafana.com>
2025-06-10 11:39:15 +0100

    add scaffolding for translations

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,11 @@
 import { DataSourcePlugin } from '@grafana/data';
+import { initPluginTranslations } from '@grafana/i18n';
 import { PromQueryEditorByApp, PrometheusDatasource, PromCheatSheet } from '@grafana/prometheus';
 
 import { ConfigEditor } from './configuration/ConfigEditor';
+import pluginJson from './plugin.json';
+
+initPluginTranslations(pluginJson.id);
 
 export const plugin = new DataSourcePlugin(PrometheusDatasource) 
   .setQueryEditor(PromQueryEditorByApp)
