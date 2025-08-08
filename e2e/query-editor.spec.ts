1724ea2ee555970f1ffb7a44cff91016246bd7c5
Andrew Hackmann <ajhack93@gmail.com>
2025-08-08 14:23:11 -0500

    fixing e2e tests

--- e2e/query-editor.spec.ts
+++ e2e/query-editor.spec.ts
@@ -1,8 +1,8 @@
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { selectors } from '@grafana/e2e-selectors';
-import { expect, test } from '@grafana/plugin-e2e';
+import { test, expect } from '@grafana/plugin-e2e';
 import { PromOptions } from '@grafana/prometheus';
-
+import semver from 'semver';
 const codeEditorProvFile = 'code-editor.yml';
 
 test.describe('Prometheus query editor', () => {
@@ -16,7 +16,7 @@ test.describe('Prometheus query editor', () => {
     step
     type
     exemplars
-  `, async ({ readProvisionedDataSource, explorePage, page }) => {
+  `, async ({ readProvisionedDataSource, explorePage, grafanaVersion, page }) => {
     const ds = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
       fileName: 'datasources.yml',
     });
@@ -55,17 +55,26 @@ test.describe('Prometheus query editor', () => {
       explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.format)
     ).toBeVisible();
 
-    // TODO: use selectors when  <EditorSwitch/> is fixed to pass data-testid
-    await expect(page.getByText('Min step')).toBeVisible();
-
     // type
     await expect(
       explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.type)
     ).toBeVisible();
 
-    // exemplars
-    // TODO: use selectors when  <EditorSwitch/> is fixed to pass data-testid
-    await expect(page.getByText('Exemplars')).toBeVisible();
+    if (semver.gte(grafanaVersion, '12.1.0')) {
+      // min step
+      await expect(
+        explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.step)
+      ).toBeVisible();
+      // exemplars
+      await expect(
+        explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.exemplars)
+      ).toBeVisible();
+    } else {
+      // min step
+      await expect(page.getByText('Min step')).toBeVisible();
+      // exemplars
+      await expect(page.getByText('Exemplars')).toBeVisible();
+    }
   });
 
   test.describe('Code editor', () => {
@@ -228,58 +237,61 @@ test.describe('Prometheus query editor', () => {
     test('it navigates to the query builder with default editor type as builder', async ({
       readProvisionedDataSource,
       explorePage,
+      grafanaVersion,
       page,
     }) => {
-      const dsDefaultEditorBuilder = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
+      const ds = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
         fileName: 'datasources.yml',
       });
 
-      await explorePage.goto();
+      await explorePage.datasource.set(ds.name);
+      if (semver.lt(grafanaVersion, '12.0.0')) {
+        await page.getByLabel('Metric').isVisible();
+      } else {
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .isVisible();
 
-      await explorePage.datasource.set(dsDefaultEditorBuilder.name);
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .focus();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .isVisible();
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .isEnabled();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .focus();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .isEnabled();
-
-      await expect(
-        explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-      ).toBeVisible();
+        await expect(
+          explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+        ).toBeVisible();
+      }
     });
 
     test('the query builder contains metric select, label filters and operations', async ({
       readProvisionedDataSource,
       explorePage,
       page,
+      grafanaVersion,
     }) => {
-      const dsDefaultEditorBuilder = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
+      const ds = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
         fileName: 'datasources.yml',
       });
 
-      await explorePage.goto();
+      await explorePage.datasource.set(ds.name);
+      if (semver.lt(grafanaVersion, '12.0.0')) {
+        await page.getByLabel('Metric').isVisible();
+      } else {
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .isEnabled();
 
-      await explorePage.datasource.set(dsDefaultEditorBuilder.name);
+        await expect(
+          explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+        ).toBeVisible();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .isEnabled();
-
-      await expect(
-        explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-      ).toBeVisible();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .focus();
-
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .focus();
+      }
       await expect(explorePage.getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect)).toBeVisible();
 
       await explorePage.getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect).focus();
@@ -295,108 +307,38 @@ test.describe('Prometheus query editor', () => {
       await expect(explorePage.getByGrafanaSelector(selectors.components.QueryBuilder.valueSelect)).toBeVisible();
     });
 
-    /*    test('it can select a metric and provide a hint', async ({
-      readProvisionedDataSource,
-      explorePage,
-      page,
-    }) => {
-      // const dsDefaultEditorBuilder = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({ fileName: 'datasources.yml' });
-
-      await explorePage.goto();
-
-      // await explorePage.datasource.set(dsDefaultEditorBuilder.name);
-
-      await page.getByTestId('data-testid Select a data source').click();
-
-      await page.getByTestId('data-testid Select a data source').fill('Azure Managed Service for Prometheus');
-
-      await page.getByRole('button', { name: 'Azure Managed Service for Prometheus' }).click();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect).isVisible();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect).isEnabled();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect).focus();
-
-      await explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect).click()
-
-      await page.getByText(metric, { exact: true }).click();
-
-      const hintText = await explorePage.getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.hints).textContent();
-
-      expect(hintText).toContain('hint: add rate');
-    });
-*/
-
-    /*   test('it can select a label filter and run a query', async ({
-      readProvisionedDataSource,
-      explorePage,
-      page,
-    }) => {
-      // const dsDefaultEditorBuilder = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({ fileName: 'datasources.yml' });
-
-      await explorePage.goto();
-
-      // await explorePage.datasource.set(dsDefaultEditorBuilder.name);
-
-      await page.getByTestId('data-testid Select a data source').click();
-
-      await page.getByTestId('data-testid Select a data source').fill('Azure Managed Service for Prometheus');
-
-      await page.getByRole('button', { name: 'Azure Managed Service for Prometheus' }).click();
-      
-      await explorePage
-        .getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect).isVisible();
-      
-      await explorePage
-        .getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect).isEnabled();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect).focus();
-        
-      await explorePage.getByGrafanaSelector(selectors.components.QueryBuilder.labelSelect).click()
-
-      await page.getByText('__name__', { exact: true }).click();
-
-      await explorePage.getByGrafanaSelector(selectors.components.QueryBuilder.valueSelect).click();
-
-      await page.getByText(metric, { exact: true }).click();
-
-      await explorePage.runQuery();
-    });
-*/
     // TODO query for metrics explorer button once prometheusUsesCombobox is GA and enabled by default
     test('it should have the metrics explorer opened via the metric select', async ({
       readProvisionedDataSource,
       explorePage,
+      grafanaVersion,
       page,
     }) => {
-      const dsDefaultEditorBuilder = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
+      const ds = await readProvisionedDataSource<DataSourcePluginOptionsEditorProps<PromOptions>>({
         fileName: 'datasources.yml',
       });
 
-      await explorePage.goto();
+      await explorePage.datasource.set(ds.name);
 
-      await explorePage.datasource.set(dsDefaultEditorBuilder.name);
+      if (semver.lt(grafanaVersion, '12.0.0')) {
+        await page.getByLabel('Metric').isVisible();
+      } else {
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .isVisible();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .isVisible();
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .isEnabled();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .isEnabled();
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .focus();
 
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .focus();
-
-      await explorePage
-        .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
-        .click();
+        await explorePage
+          .getByGrafanaSelector(selectors.components.DataSource.Prometheus.queryEditor.builder.metricSelect)
+          .click();
+      }
 
       // await page.getByText('Metrics explorer', { exact: true }).click();
 
